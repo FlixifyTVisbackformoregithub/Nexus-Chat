9cd8029a3348e49608e40cec3866e7499c4d62f4
Flix <flixgit@outlook.com>
2024-10-29 16:43:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVEaivv2KHC7A3YItwM6rTK7ZOlS68hWs",
+  authDomain: "chatting-1d287.firebaseapp.com",
+  projectId: "chatting-1d287",
+  storageBucket: "chatting-1d287.appspot.com",
+  messagingSenderId: "653815240754",
+  appId: "1:653815240754:web:7b44f598b312bea71ec42b",
+  measurementId: "G-23RPZDMDRW"
+};
